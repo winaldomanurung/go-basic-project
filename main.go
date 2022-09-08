44ece52c85203efc12b3b10d2970c7e221181a25
winaldomanurung <winaldo_satryadi@rocketmail.com>
2022-09-08 20:33:45 +0700

    refactor and add style

--- main.go
+++ main.go
@@ -2,33 +2,11 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 )
 
 // Jika mau deploy harus hapus 127.0.0.1
 const portNumber = "127.0.0.1:8080"
-
-
-func Home(w http.ResponseWriter, r *http.Request){
-	renderTemplate(w, "home.page.tmpl")
-}
-
-func About(w http.ResponseWriter, r *http.Request){
-	renderTemplate(w, "about.page.tmpl")
-}
-
-func renderTemplate (w http.ResponseWriter, tmpl string){
-	// kita parse salah satu template
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-
-	err := parsedTemplate.Execute(w, nil)
-	if err !=nil {
-		fmt.Println("error parsing template:", err)
-		return
-	}
-}
-
 
 func main() {
 	http.HandleFunc("/", Home)
