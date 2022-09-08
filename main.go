40b59fef5d981c181d50a712f44d50d9fc425398
winaldomanurung <winaldo_satryadi@rocketmail.com>
2022-09-08 14:33:45 +0700

    error handler

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
 
-const portNumber = ":8080"
+// Jika mau deploy harus hapus 127.0.0.1
+const portNumber = "127.0.0.1:8080"
 
 // Ini adalah handlerfunction
 // Ingat bahwa handlerfunction HARUS handle two parameters, responsewriter dan request
@@ -14,19 +16,40 @@ func Home(w http.ResponseWriter, r *http.Request){
 }
 
 func About(w http.ResponseWriter, r *http.Request){
-	sum := AddValues(2,2)
+	sum := addValues(2,2)
 	_, _ = fmt.Fprintf(w, fmt.Sprintf("2+2 is %d", sum))
 }
 
 // what if I pass x and y that is not integer?
 // kita handle error dengan tambah return error
-func AddValues (x,y int) int {
+func addValues (x,y int) int {
 	return x + y
+}
+
+func Divide(w http.ResponseWriter, r *http.Request){
+	// kita ga ingin melakukan pembagian dengan 0
+	f, err := divideValues(100.0, 0.0)
+	if err != nil {
+		fmt.Fprintf(w, "cannot divide by zero")
+		return
+	}
+	fmt.Fprintf(w, fmt.Sprintf("%f divided by %f is %f", 100.0, 0.0,f))
+
+}
+
+func divideValues(x,y float32) (float32,error){
+	if y <= 0 {
+		err := errors.New("cannot divide by zero")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
 }
 
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
 
