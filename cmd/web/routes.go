3dbf128a415928b3d78718b18392146270a495c2
winaldomanurung <winaldo_satryadi@rocketmail.com>
2022-09-23 20:30:02 +0700

    chi route and middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -3,16 +3,24 @@ package main
 import (
 	"net/http"
 
-	"github.com/bmizerany/pat"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/winaldomanurung/go-basic-web-app/pkg/config"
 	"github.com/winaldomanurung/go-basic-web-app/pkg/handlers"
 )
 
 func routes(app *config.AppConfig) http.Handler {
-	mux := pat.New()
+	// mux := pat.New()
 
-	mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
-	mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
+	// mux.Get("/", http.HandlerFunc(handlers.Repo.Home))
+	// mux.Get("/about", http.HandlerFunc(handlers.Repo.About))
 
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+	mux.Use(NoSurf)
+
+	mux.Get("/", handlers.Repo.Home)
+	mux.Get("/about", handlers.Repo.About)
 	return mux
 }
